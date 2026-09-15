b0855a828d31aebd5a0a1cff4a16d318d6c9343e
Siyath Koralage <siyathkoralage07@gmail.com>
2026-09-15 10:40:07 +0530

    Introduction to JavaScrpit and Assigning Variables

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//HELLO TO JavaScript!!!
+console.log("hello, world");
+
+// sending alerts
+alert("hi icet :")
+
+// no need to use a semi-colon to define lines
+console.log("hi.....hi.....hi");
+
+//Assigning Variables(in js the default setting for variables is non-type_strict, AND all variable names are CASE SENSITIVE soooo (eg. "name" and "NAME" are two different variables))
+let name = "saman";
+let age = "20";
+let afterAL = true;
+let customer = []; // an ARRAY
+let cashier = {}; // an OBJECT
+
+console.log(typeof name);
+console.log(age);
+console.log(afterAL);
+console.log(typeof customer);
+console.log(cashier);
+
